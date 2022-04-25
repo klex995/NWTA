6891bfa3c200182f6731c3b563fcc88fd1244781
snipspod <jast99@gmail.com>
2022-04-25 19:27:50 +0200

    corrected typo

--- models/Cart.js
+++ models/Cart.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const { boolean } = require("webidl-conversions");
 
-const CroductSchema = new mongoose.Schema(
+const ProductSchema = new mongoose.Schema(
     {
         userId: {type: String, required: true},
         products: [
