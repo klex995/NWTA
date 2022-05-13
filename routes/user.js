a3adf272134985867752f077cf660ee1a2081e88
snipspod <jast99@gmail.com>
2022-05-13 12:09:37 +0200

    added deleting, get, stats

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const User = require("../models/User");
-const { verifyToken, verifyTokenAndAuthorization } = require("./verifyToken");
+const { verifyToken, verifyTokenAndAuthorization, verifyTokenAndAdmin } = require("./verifyToken");
 
 router.get("/usertest", (req, res)=>{
     res.send("user test is successfull");
@@ -10,6 +10,8 @@ router.post("/userposttest", (req,res)=>{
     const username = req.body.username
     console.log(username);
 })
+
+//UPDATE
 
 router.put("/:id",verifyTokenAndAuthorization , async (req,res) => {
     if(req.body.password){
@@ -26,5 +28,67 @@ router.put("/:id",verifyTokenAndAuthorization , async (req,res) => {
         res.status(500).json(err);
     }
 })
+
+//DELETE
+
+router.delete("/:id", verifyTokenAndAuthorization, async (req,res) => {
+    try{
+        await User.findByIdAndDelete(req.params.id)
+        res.status(200).json("User has been deleted!")
+    }catch(err){
+        res.status(500).json("Error");
+    }
+});
+
+//GET
+
+router.get("/find/:id", verifyTokenAndAdmin, async (req,res) => {
+    try{
+        const user = await User.findById(req.params.id)
+        const { password, ...others } = user._doc;
+        res.status(200).json(others)
+    }catch(err){
+        res.status(500).json("Error");
+    }
+});
+
+//GET ALL USERS
+
+router.get("/", verifyTokenAndAdmin, async (req,res) => {
+    const query = req.query.new;
+    try{
+        const users = query ? await User.find().sort({ _id: -1 }).limit(5) : await User.find()
+        res.status(200).json(users)
+    }catch(err){
+        res.status(500).json("Error");
+    }
+});
+
+//GET USER STATS
+
+router.get("/stats", verifyTokenAndAdmin, async (req,res) => {
+    const date = new Date();
+    const lastYear = new Date(date.setFullYear(date.getFullYear() - 1));
+
+    try{
+        const data = await User.aggregate([
+            { $match: { createdAt: { $gte: lastYear } } },
+            {
+              $project: {
+                month: { $month: "$createdAt" },
+              },
+            },
+            {
+              $group: {
+                _id: "$month",
+                total: { $sum: 1 },
+              },
+            },
+          ]);
+          res.status(200).json(data)
+    }catch(err){
+        res.status(500).json("Error!")
+    }
+});
 
 module.exports = router
