81d71543fec4961e8660453a2906b215fe835161
MateuszStaron <104312077+MateuszStaron@users.noreply.github.com>
2022-05-30 15:01:08 +0200

    Update Product.js
    adjusted to the type of products

--- models/Product.js
+++ models/Product.js
@@ -7,8 +7,7 @@ const ProductSchema = new mongoose.Schema(
         desc: {type: String, required: true},
         img: {type: String, required: true},
         categories: {type: Array},
-        size: {type: String},
-        color: {type: String},
+        typ: {type: String},
         price: {type: Number, required: true},
     },
     { timestamps: true }
