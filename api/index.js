803f0f189764d87d6eea0b8fa746551c2e45665d
mateusz staron <mati8staron@gmail.com>
2022-05-30 20:24:25 +0200

    added 'cors' to server to allow reading port with data; added fetching and filtering of products in client app

--- api/index.js
+++ api/index.js
@@ -7,17 +7,18 @@ const authRoute = require("./routes/auth");
 const productRoute = require("./routes/product");
 const cartRoute = require("./routes/cart");
 const orderRoute = require("./routes/order");
-
+const cors = require("cors");
 
 dotenv.config();
 
 mongoose
-    .connect(process.env.MONGO_URL)
-    .then(() => console.log("DB Connection Successfull!"))
-    .catch((err) => {
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("DB Connection Successfull!"))
+  .catch((err) => {
     console.log(err);
-});
+  });
 
+app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
@@ -26,10 +27,9 @@ app.use("/api/orders", orderRoute);
 app.use("/api/carts", cartRoute);
 
 app.get("/api/test", () => {
-   console.log("Test is successfull") 
-})
+  console.log("Test is successfull");
+});
 
-
-app.listen(process.env.PORT || 5000, () => { 
-    console.log("Backend server is running!");
-})
+app.listen(process.env.PORT || 5000, () => {
+  console.log("Backend server is running!");
+});
--- client/src/pages/ProductList.jsx
+++ client/src/pages/ProductList.jsx
@@ -4,6 +4,8 @@ import News from "../components/News";
 import Products from "../components/Products";
 import Newsletter from "../components/Newsletter";
 import Footer from "../components/Footer";
+import { useLocation } from "react-router-dom";
+import { useState } from "react";
 
 const Container = styled.div``;
 
@@ -33,15 +35,28 @@ const Select = styled.select`
 const Option = styled.option``;
 
 const ProductList = () => {
+  const location = useLocation();
+  const cat = location.pathname.split("/")[2];
+  const [filter, setFilters] = useState({});
+  const [sort, setSort] = useState("newest");
+
+  const handleFilters = (e) => {
+    const value = e.target.value;
+    setFilters({
+      ...filter,
+      [e.target.name]: value
+    });
+  };
+  console.log(filter);
   return (
     <Container>
       <Navbar />
       <News />
-      <Title>Proszek</Title>
+      <Title>{cat}</Title>
       <FilterContainer>
         <Filter>
           <FilterText>Filtruj produkty:</FilterText>
-          <Select>
+          <Select name="type" onChange={handleFilters}>
             <Option disabled selected>
               Typ
             </Option>
@@ -52,14 +67,14 @@ const ProductList = () => {
         </Filter>
         <Filter>
           <FilterText>Sortuj produkty:</FilterText>
-          <Select>
-            <Option selected>Najnowsze</Option>
-            <Option>Cena rosnąco</Option>
-            <Option>Cena malejąco</Option>
+          <Select onChange={(e) => setSort(e.target.value)}>
+            <Option value="newest">Najnowsze</Option>
+            <Option value="asc">Cena rosnąco</Option>
+            <Option value="desc">Cena malejąco</Option>
           </Select>
         </Filter>
       </FilterContainer>
-      <Products />
+      <Products cat={cat} filter={filter} sort={sort} />
       <Newsletter />
       <Footer />
     </Container>
